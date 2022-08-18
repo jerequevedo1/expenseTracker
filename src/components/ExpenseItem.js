af57d29148e89743cf5f52f9dd1d2affff35472b
Jeremias Quevedo <jeremias22xt@gmail.com>
2022-08-18 16:19:20 -0300

    styles and logic were added to ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,24 @@
-function ExpenseItem(){
-    return <h2>Expense item!</h2>
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+
+    const month = props.date.toLocaleString('es-AR', { month: 'long' });
+    const day = props.date.toLocaleString('es-AR', { day: '2-digit' });
+    const year = props.date.getFullYear();
+
+    return (
+        <div className='expense-item'>
+            <div>
+                <div>{day}</div>
+                <div>{month}</div>
+                <div>{year}</div>
+            </div>
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
