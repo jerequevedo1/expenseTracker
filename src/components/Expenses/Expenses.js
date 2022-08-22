87b4ea35472207ada7b716468a93b2b92f8676d8
Jeremias Quevedo <jeremias22xt@gmail.com>
2022-08-22 15:16:09 -0300

    addExpenseHandler method was changed

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,16 +14,11 @@ const Expenses = (props) => {
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onYearFilter={yearToFilter} />
-            <ExpenseItem
-                title={props.items[0].title}
-                amount={props.items[0].amount}
-                date={props.items[0].date}
-            />
-            <ExpenseItem
-                title={props.items[1].title}
-                amount={props.items[1].amount}
-                date={props.items[1].date}
-            />
+            {props.items.map(expense =>
+                <ExpenseItem
+                    title={expense.title}
+                    amount={expense.amount}
+                    date={expense.date} />)}
         </Card>
     );
 }
